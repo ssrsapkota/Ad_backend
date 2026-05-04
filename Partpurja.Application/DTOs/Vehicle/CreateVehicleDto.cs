b9b8c4ed07ef1fabcf70d9f48f597954e1dae4fa
NitikGit <nimanitik@gmail.com>
2026-05-04 18:54:18 +0545

    Add Vehicle Model

---
+++ Partpurja.Application/DTOs/Vehicle/CreateVehicleDto.cs
@@ -0,0 +1,21 @@
+namespace Partpurja.Application.DTOs.Vehicle
+{ 
+   public class CreateVehicleDto
+{
+    public int CustomerId { get; set; }
+
+    public string RegistrationNumber { get; set; } = string.Empty;
+
+    public string Brand { get; set; } = string.Empty;
+
+    public string Model { get; set; } = string.Empty;
+
+    public int Year { get; set; }
+
+    public string ChassisNumber { get; set; } = string.Empty;
+
+    public string VehicleCondition { get; set; } = string.Empty;
+
+    public int MonthlyUsageKm { get; set; }
+}
+}
